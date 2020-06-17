fc23c19f81b31a766a829b365db6231032697404
trackrun82 <64228800+trackrun82@users.noreply.github.com>
2020-06-17 17:51:17 -0600

    Finished WeatherPy coding

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -2,4 +2,4 @@
 weather_api_key = "96e6b69ec45084357cc435def1b4cb41"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyC5FbiCyokU94_LIAu5KnrygwsPiUXyuYs"
--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,2 @@
 # OpenWeatherMap API Key
 weather_api_key = "96e6b69ec45084357cc435def1b4cb41"
-
-# Google API Key
-g_key = "YOUR KEY HERE!"
